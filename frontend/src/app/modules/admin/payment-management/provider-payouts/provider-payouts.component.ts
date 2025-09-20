ed8dfda1777ef2cb60b3e88168a0735dcebfbd91
rohitjangid01 <rohit.devteam@webdaire.com>
2025-09-20 23:19:41 +0530

    admin pages setup

---
+++ frontend/src/app/modules/admin/payment-management/provider-payouts/provider-payouts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-payouts',
+  imports: [],
+  templateUrl: './provider-payouts.component.html',
+  styleUrl: './provider-payouts.component.scss'
+})
+export class ProviderPayoutsComponent {
+
+}
---
+++ frontend/src/app/modules/admin/service-management/category-service/category-service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-service',
+  imports: [],
+  templateUrl: './category-service.component.html',
+  styleUrl: './category-service.component.scss'
+})
+export class CategoryServiceComponent {
+
+}
