b552f2f6512696d8d5d5631bf8ef3a003951abfd
rohitjangid01 <rohit.devteam@webdaire.com>
2025-09-20 23:51:22 +0530

    marketing manager pages setup

---
+++ frontend/src/app/modules/marketing-manager/campaign-management/referral-campaign/referral-campaign.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-referral-campaign',
+  imports: [],
+  templateUrl: './referral-campaign.component.html',
+  styleUrl: './referral-campaign.component.scss'
+})
+export class ReferralCampaignComponent {
+
+}
---
+++ frontend/src/app/modules/marketing-manager/reports/user-engagement-reports/user-engagement-reports.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-engagement-reports',
+  imports: [],
+  templateUrl: './user-engagement-reports.component.html',
+  styleUrl: './user-engagement-reports.component.scss'
+})
+export class UserEngagementReportsComponent {
+
+}
