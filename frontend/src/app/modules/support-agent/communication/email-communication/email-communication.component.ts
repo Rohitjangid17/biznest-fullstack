ba1ec3a4b03eadaac1f94fe249d922792e5666a9
rohitjangid01 <rohit.devteam@webdaire.com>
2025-09-20 23:32:32 +0530

    support agent pages setup

---
+++ frontend/src/app/modules/support-agent/communication/email-communication/email-communication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-communication',
+  imports: [],
+  templateUrl: './email-communication.component.html',
+  styleUrl: './email-communication.component.scss'
+})
+export class EmailCommunicationComponent {
+
+}
---
+++ frontend/src/app/modules/support-agent/ticket-management/escalation/escalation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escalation',
+  imports: [],
+  templateUrl: './escalation.component.html',
+  styleUrl: './escalation.component.scss'
+})
+export class EscalationComponent {
+
+}
